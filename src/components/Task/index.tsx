8f53403e3c274980b8458ee806484511d81a49a3
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-03-06 23:06:04 -0300

    feat: change styles and organize layout

---
+++ src/components/Task/index.tsx
@@ -0,0 +1,43 @@
+import { DotsThreeVertical } from "@phosphor-icons/react";
+import { DraggableProvided, DraggableStateSnapshot } from "react-beautiful-dnd"
+import { ITask } from "../../interfaces/ITodo"
+import { Container, Content, Heading, Status, Text } from "./styles";
+
+type Props = {
+  provided: DraggableProvided;
+  snapshot: DraggableStateSnapshot;
+  task: ITask;
+} 
+
+export function Task({provided, snapshot, task}: Props) {
+
+  function handleOnClick() {
+    console.log('clicou');
+    console.log(provided);
+  }
+
+  return (
+    <Container 
+      ref={provided.innerRef}
+      {...provided.draggableProps}
+      {...provided.dragHandleProps} 
+      isDragging={snapshot.isDragging}
+      draggingStyles={provided.draggableProps}     
+      onClick={handleOnClick}
+    > 
+      <Status type={task.status} />
+      
+      <Content>
+        <div className="header">
+          <Heading>{task.title}</Heading>
+
+          <button>
+            <DotsThreeVertical size={18} weight="bold" />
+          </button>
+        </div>
+
+        {task.description ? <Text>{task.description}</Text> : <Text />}
+      </Content>
+    </Container>
+  )
+}
---
+++ src/components/Task/styles.ts
@@ -0,0 +1,71 @@
+import { DraggableProvidedDraggableProps, DraggingStyle, NotDraggingStyle } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+interface Props {
+  isDragging: boolean;
+  draggingStyles: DraggableProvidedDraggableProps;
+}
+
+export const Container = styled.div<Props>`
+  width: 300px;
+  min-height: 60px;
+
+  color: #363636;
+  border-radius: 4px; 
+  background-color: #fff;
+  /* border-color: ${({isDragging}) => isDragging ? "red" : "transparent"}; */
+  box-shadow: ${({isDragging}) => isDragging ? "none" : "0px 1px 1px rgba(0, 0, 0, 0.1)"};
+  ${({draggingStyles}) => ({...draggingStyles.style})}
+
+  display: flex;
+  justify-content: center;
+  
+`;
+
+interface StatusProps {
+  type: "in progress" | "completed"
+}
+
+export const Status = styled.div<StatusProps>`
+  width: 7px;
+  height: 100%;
+  margin-bottom: 8px;
+  background-color: ${({type}) => type === "completed" ? "#00B37E" : "#FBA94C"};
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+`;
+
+export const Content = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 8px;
+
+  .header {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 4px;
+
+    button {
+      all: unset;
+      cursor: pointer;
+    }
+  }
+`;
+
+export const Heading = styled.h2`
+  color: #344449;
+  font-size: 16px;
+  font-weight: 500;
+  letter-spacing: 0.05em;
+`;
+
+export const Text = styled.p`
+  color: #718797;
+  font-size: 12px;
+  height: 18px;
+`;
+
